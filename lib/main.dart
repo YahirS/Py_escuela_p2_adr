d787799d103b05626de2b5113df64c64aa82c18b
FlutLab.io <>
2021-05-10 19:39:17 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -98,6 +98,27 @@ class Inicio extends StatelessWidget {
                 ), // fin de colum
               ],
             ), //fin de row
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/empresa");
+                    },
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Center(
+                        child: Text("EMPRESA", textAlign: TextAlign.center),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ), //fin de comuln 2
           ], //fin de widge
         ), //fin caja
       ), //fin de container
